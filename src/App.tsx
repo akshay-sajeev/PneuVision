62bbe8a8e21243d530dc9782cfdabe135801c2a0
shriya-1203 <ishriya12@gmail.com>
2025-10-09 20:19:36 -0500

    Added navbar and basic login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css';
+import './styles/navbar.css'
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="app-root">
+  {/* Header Banner */}
+  <header className="app-header">
+    <h1 className="app-title">PneuVision</h1>
+    <nav>
+      <button className="nav-link btn-home">Home</button>
+      <button className="nav-link">About</button>
+    </nav>
+  </header>
+
+  {/* Main Section */}
+  <main className="container">
+    <div className="login-card">
+      <input
+        className="pill-input"
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        className="pill-input"
+        type="password"
+        placeholder="Password"
+        style={{ marginTop: '1rem' }}
+      />
+      <button className="signin-btn" style={{ marginTop: '2.5rem' }}>Sign in</button>
+    </div>
+  </main>
+</div>
+  );
 }
 
-export default App
+export default App;
