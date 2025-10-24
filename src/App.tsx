c4c9f71852d4700fc796f6cff257d92d3cd7e60a
Andres Torres <et.andres744@gmail.com>
2025-10-24 14:23:15 -0500

    Revert "Frontend with router"
    This reverts commit fbfe7420c185081d411fe4292f75159d38f0d657.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
@@ -24,6 +25,41 @@ function App() {
         </main>
       </div>
     </BrowserRouter>
+=======
+import './styles/App.css';
+import './styles/navbar.css'
+
+function App() {
+  return (
+    <div className="app-root">
+  {/* Header Banner */}
+  <header className="app-header">
+    <h1 className="app-title">PneuVision</h1>
+    <nav>
+      <button className="nav-link btn-home">Home</button>
+      <button className="nav-link">About</button>
+    </nav>
+  </header>
+
+  {/* Main Section */}
+  <main className="container">
+    <div className="login-card">
+      <input
+        className="pill-input"
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        className="pill-input"
+        type="password"
+        placeholder="Password"
+        style={{ marginTop: '1rem' }}
+      />
+      <button className="signin-btn" style={{ marginTop: '2.5rem' }}>Sign in</button>
+    </div>
+  </main>
+</div>
+>>>>>>> parent of fbfe7420 (Frontend with router)
   );
 }
 
--- src/pages/About.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div>
-      <h2>About PneuVision</h2>
-      <p>Our project uses AI to detect pneumonia from chest X-rays.</p>
-    </div>
-  );
-}
