8c2397fb6e71f82b69fcb60d6a38350f436c6f31
akshay-sajeev <asajeev2006@gmail.com>
2025-10-31 21:39:59 -0500

    Readded results and uploads folders and fixed upload scan button on homepage

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,27 +1,28 @@
 import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-      <div> 
-        <main className="container">
-          <div className="login-card">
-            <p className="home-description">
-              <strong>PneuVision</strong> is a clinical support tool designed 
-              to assist healthcare professionals in evaluating chest X-rays for 
-              signs of pneumonia. Our AI-powered system provides rapid analysis and 
-              visual heatmaps to highlight regions of interest, helping doctors 
-              confirm diagnoses, identify areas that may have been overlooked, 
-              and make more informed decisions with confidence. This platform is 
-              intended to complement, not replace, clinical expertise, enhancing 
-              accuracy and efficiency in diagnostic workflows.
-            </p>  
-            <Link to="/upload">
-              <button className="signin-btn">Upload Scan</button>
-            </Link>
-          </div>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div> 
+      <main className="container">
+        <div className="login-card">
+          <p className="home-description">
+            <strong>PneuVision</strong> is a clinical support tool designed 
+            to assist healthcare professionals in evaluating chest X-rays for 
+            signs of pneumonia. Our AI-powered system provides rapid analysis and 
+            visual heatmaps to highlight regions of interest, helping doctors 
+            confirm diagnoses, identify areas that may have been overlooked, 
+            and make more informed decisions with confidence. This platform is 
+            intended to complement, not replace, clinical expertise, enhancing 
+            accuracy and efficiency in diagnostic workflows.
+          </p>  
 
-  export default Home;
+          <Link to="/upload">
+            <button className="signin-btn home-upload-btn">Upload Scan</button>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import ProfileBar from "./components/ProfileBar";
 import Navbar from "./components/Navbar";
 import "./styles/App.css";
@@ -8,8 +7,11 @@ function Login() {
     <div className="app-root">
       <Navbar />
       <ProfileBar />
+
       <main className="container">
         <div className="login-card">
+          <h2 className="login-title">Login</h2>
+          
           <input
             className="pill-input"
             type="text"
@@ -21,7 +23,9 @@ function Login() {
             placeholder="Password"
             style={{ marginTop: '1rem' }}
           />
-          <button className="signin-btn" style={{ marginTop: '2.5rem' }}>Sign in</button>
+          <button className="signin-btn" style={{ marginTop: '2.5rem' }}>
+            Sign In
+          </button>
         </div>
       </main>
     </div>
